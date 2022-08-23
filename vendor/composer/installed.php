06583eb6a33a7c068220dade701cb99d899c1a83
solaris-isbc <solaris@interstellarbroadcasting.center>
2022-08-23 18:36:34 +0200

    added f3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '6b7caaba14829e0781e45b91df3aef8d8f41c242',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '6b7caaba14829e0781e45b91df3aef8d8f41c242',
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'reference' => 'cdd878db018280369b643fce62f70e55044f9ac4',
+            'dev_requirement' => false,
+        ),
+    ),
+);
